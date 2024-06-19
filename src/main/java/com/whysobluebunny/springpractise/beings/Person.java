c8d4496209c9802aaa7c99e9f33d26bf1c55bb50
whysobluebunny <wsb.bart@gmail.com>
2024-06-19 14:30:57 +0300

    | IoC_DI | 8. DI с помощью сеттера |

--- src/main/java/com/whysobluebunny/springpractise/beings/Person.java
+++ src/main/java/com/whysobluebunny/springpractise/beings/Person.java
@@ -3,8 +3,18 @@ package com.whysobluebunny.springpractise.beings;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
-        System.out.println("Person created");
+    public Person() {
+        System.out.println("Person() created");
+    }
+
+    //    public Person(Pet pet) {
+//        System.out.println("Person(pet) created");
+//        this.pet = pet;
+//    }
+
+    // pet -> setPet
+    public void setPet(Pet pet) {
+        System.out.println("Person: Set pet");
         this.pet = pet;
     }
 
